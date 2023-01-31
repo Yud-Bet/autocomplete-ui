c7b942d71d51627c72c4a0fbb3f3b414ac8a6529
Yud Bet <66458858+Yud-Bet@users.noreply.github.com>
2023-01-31 22:25:32 +0700

    Fix UI, add debounce, install font

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,46 +1,64 @@
-import React, { useState } from 'react';
-import './SearchBar.css';
-import SearchIcon from '@mui/icons-material/Search';
-import axios from 'axios';
+import React, { useCallback, useState } from "react";
+import "./SearchBar.css";
+import SearchIcon from "@mui/icons-material/Search";
+import axios from "axios";
+import { debounce } from "@mui/material";
 
-axios.defaults.baseURL = process.env.REACT_APP_API_ENDPOINT
+axios.defaults.baseURL = process.env.REACT_APP_API_ENDPOINT;
 
-function SearchBar({placeholder}) {
+function SearchBar({ placeholder }) {
   const [filteredData, setFilteredData] = useState([]);
+
+  const debounceSuggestion = useCallback(
+    debounce(async (query) => {
+      console.log("Helllo", query);
+      const response = await axios.get(`/search?q=${query}`);
+      setFilteredData(response.data);
+    }, 50),
+    []
+  );
 
   const handleFilter = async (event) => {
     const query = event.target.value;
     if (query) {
-      const newFilter = (await axios.get(`/search?q=${query}`)).data;
-      setFilteredData(newFilter);
+      await debounceSuggestion(query);
     } else setFilteredData([]);
-  }
+  };
 
   const handleSubmit = async (event) => {
     const query = event.target.value;
     console.log(query);
-  }
+  };
 
   return (
-    <div className='search'>
-      <div className='searchInputs'>
-      <div className='searchIcon'>
-          <SearchIcon/>
+    <div className="search">
+      <div className="searchInputs">
+        <div className="searchIcon">
+          <SearchIcon />
         </div>
-        <input type="text" placeholder={placeholder} onChange={handleFilter} onSubmit={handleSubmit} />
-
+        <input
+          type="text"
+          placeholder={placeholder}
+          onChange={handleFilter}
+          onSubmit={handleSubmit}
+        />
       </div>
-      {
-        filteredData.length !== 0 && (
-          <div className='dataResult'>
-            {filteredData.map((value) => {
-              return <a className='dataItem' href={value}><p>{value}</p></a>
-            })}
-          </div>
-        )
-      }
+      {filteredData.length !== 0 && (
+        <div className="dataResult">
+          {filteredData.map((value, index) => {
+            return (
+              <a key={`suggestion_${index}`} className="dataItem" href={value}>
+                <div className="searchSuggestionIcon">
+                  <SearchIcon />
+                </div>
+                <p>{value}</p>
+              </a>
+            );
+          })}
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default SearchBar
+export default SearchBar;
