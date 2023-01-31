dca680b59967892487521ffb5625982986761254
Yud Bet <66458858+Yud-Bet@users.noreply.github.com>
2023-01-31 17:17:10 +0700

    UI enhancement, set title

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Logo from './Images/Ceeit.png';
 function App() {
   return (
 <div className='App'>
-  <img src={Logo} width ="382" height="206"></img>
+  <img src={Logo} alt="logo" width ="382" height="206"></img>
   <SearchBar placeholder='Search here'/></div>
   );
 }
